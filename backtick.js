ae57111bff8f7f1590776bbd8e8a89e08a4e721a
Danielle Smith <57499759+Danielle-Smith@users.noreply.github.com>
2020-03-03 10:40:59 -0700

    Add files via upload

---
+++ backtick.js
@@ -0,0 +1,8 @@
+//   Backtick string interpolation
+
+const lyrics = 'Never gonna give you up';
+console.log(`I'm ${lyrics}`); // you can use javascript code between {}
+
+console.log(`I'm ${2 + 2}`);
+
+console.log(`I'm ${lyrics + ' ' + lyrics}`)
